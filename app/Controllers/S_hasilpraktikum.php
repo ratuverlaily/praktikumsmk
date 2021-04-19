31ad68d554a5e244b42c58bcf92dea97270f65c5
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 12:30:38 +0700

    Update S_hasilpraktikum.php

--- app/Controllers/S_hasilpraktikum.php
+++ app/Controllers/S_hasilpraktikum.php
@@ -18,12 +18,20 @@ class S_hasilpraktikum extends BaseController
     public function index()
     {
         helper(['form', 'url']);
-        $this->Ms_hasilpraktikum = new Ms_hasilpraktikum();
-        $data['kelasaktif'] = $this->session->get('nama');
-        $data['id_kelas'] = $this->session->get('id_kelas');
-        $data['praktikums'] = $this->Ms_hasilpraktikum->get_all_hasilpraktikum($this->session->get('id_kelas'));
 
-        return view('t_siswa/hasilpraktikum', $data);
+        if (empty($this->session->get('id_kelas'))) {
+            $data['kelasaktif'] = 'BELUM TERDAFTAR';
+            $data['status_kelas'] = 0;
+
+            return view('t_siswa/kelas', $data);
+        } else {
+            $this->Ms_hasilpraktikum = new Ms_hasilpraktikum();
+            $data['kelasaktif'] = $this->session->get('nama');
+            $data['id_kelas'] = $this->session->get('id_kelas');
+            $data['praktikums'] = $this->Ms_hasilpraktikum->get_all_hasilpraktikum($this->session->get('id_kelas'));
+
+            return view('t_siswa/hasilpraktikum', $data);
+        }
     }
 
     public function posttest($id)
