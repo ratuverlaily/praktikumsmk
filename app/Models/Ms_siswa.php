a0f28c95fc007b985719b32f2e067db59e6a8ccb
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 13:09:42 +0700

    Update Ms_siswa.php

--- app/Models/Ms_siswa.php
+++ app/Models/Ms_siswa.php
@@ -20,7 +20,7 @@ class Ms_siswa extends Model
     public function get_kelas_by_user()
     {
         $id = user()->id;
-        $sql = 'SELECT b.id_kelas as id_kelas, b.kode as kode, b.nama as nama, b.jurusan as jurusan, b.jumlah as jumlah, b.id_user as id_guru FROM userdetail a INNER JOIN kelas b ON a.id_kelas = b.id_kelas WHERE a.id  = ' . $id;
+        $sql = 'SELECT b.id_kelas as id_kelas, b.kode as kode, b.nama as nama, b.jurusan as jurusan, b.jumlah as jumlah, b.id_user as id_guru FROM userdetail a INNER JOIN kelas b ON a.id_kelas = b.id_kelas WHERE a.id_user  = ' . $id;
         $query =  $this->db->query($sql);
 
         return $query->getRow();
@@ -30,5 +30,12 @@ class Ms_siswa extends Model
     {
         $this->db->table($this->table)->update($data, $where);
         //return $this->db->affectedRows();
+    }
+
+    public function get_all_siswa($id_kelas)
+    {
+        $query = $this->db->query('select * from userdetail a INNER JOIN users b ON a.id_user = b.id where id_kelas =' . $id_kelas);
+
+        return $query->getResult();
     }
 }
