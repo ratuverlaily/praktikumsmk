1e99ea5147bd458612c9f8a10f60712096b48b14
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 13:10:18 +0700

    Update Ms_kelas.php

--- app/Models/Ms_kelas.php
+++ app/Models/Ms_kelas.php
@@ -28,7 +28,7 @@ class Ms_kelas extends Model
     public function get_kelas_by_user($id)
     {
         $id = user()->id;
-        $sql = 'SELECT b.id_kelas as id_kelas, b.kode as kode, b.nama as nama, b.jurusan as jurusan, b.jumlah as jumlah, b.id_user as id_guru FROM userdetail a INNER JOIN kelas b ON a.id_kelas = b.id_kelas WHERE a.id  = ' . $id;
+        $sql = 'SELECT b.id_kelas as id_kelas, b.kode as kode, b.nama as nama, b.jurusan as jurusan, b.jumlah as jumlah, b.id_user as id_guru FROM userdetail a INNER JOIN kelas b ON a.id_kelas = b.id_kelas WHERE a.id_user  = ' . $id;
         $query =  $this->db->query($sql);
 
         return $query->getRow();
