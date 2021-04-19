a8ccd0c688a6a3eb0f111269859c8c43b606ee7b
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 12:30:55 +0700

    Update S_kelas.php

--- app/Controllers/S_kelas.php
+++ app/Controllers/S_kelas.php
@@ -21,21 +21,23 @@ class S_kelas extends BaseController
         helper(['form', 'url']);
         $id_user = user()->id;
 
-        $this->Ms_kelas = new Ms_kelas();
-        $data['kelass'] = $this->Ms_kelas->get_kelas_by_user($id_user);
-        $data['kelasaktif'] = $this->session->get('nama');
-        $data['id_kelas'] = $this->session->get('id_kelas');
-        $id_guru = $this->session->get('id_guru');
+        if (empty($this->session->get('id_kelas'))) {
+            $data['kelasaktif'] = 'BELUM TERDAFTAR';
+            $data['status_kelas'] = 0;
+        } else {
+            $this->Ms_kelas = new Ms_kelas();
+            $data['kelass'] = $this->Ms_kelas->get_kelas_by_user($id_user);
+            $data['kelasaktif'] = $this->session->get('nama');
+            $data['id_kelas'] = $this->session->get('id_kelas');
+            $id_guru = $this->session->get('id_guru');
 
-        /*$guru = $this->Ms_kelas->get_pengajar($id_guru);
-        $data['nama_guru'] =  $guru->fullname;
+            $guru = $this->Ms_kelas->get_pengajar($id_guru);
+            $data['nama_guru'] =  $guru->fullname;
 
-        $kls = $this->Ms_kelas->get_status_kelas();
-        $data['status_kelas'] =  $kls->id_kelas;*/
+            $data['status_kelas'] = 1;
+        }
 
-        dd($data);
-
-        //return view('t_siswa/kelas', $data);
+        return view('t_siswa/kelas', $data);
     }
 
     public function kelas_add()
@@ -47,12 +49,19 @@ class S_kelas extends BaseController
         $data = $this->Mg_kelas->search_kodekelas_by_id($kode);
 
         if (!empty($data->id_kelas)) {
+            $dt = json_encode($data);
+            $hsl = json_decode($dt, true);
+            $this->session->set($hsl);
+
             $this->Ms_kelas = new Ms_kelas();
             $data = array(
                 'id_kelas' => $data->id_kelas,
             );
-            $data = $this->Ms_kelas->user_update(array('id_user' => user()->id), $data);
+            $this->Ms_kelas->user_update(array('id_user' => user()->id), $data);
+
+            echo json_encode(array("status" => TRUE));
+        } else {
+            echo json_encode(array("status" => FALSE));
         }
-        echo json_encode(array("status" => TRUE));
     }
 }
