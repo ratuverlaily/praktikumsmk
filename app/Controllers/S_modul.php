fabf1e4027ee36992d3424d9f6114342235bc708
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 12:30:59 +0700

    Update S_modul.php

--- app/Controllers/S_modul.php
+++ app/Controllers/S_modul.php
@@ -17,12 +17,20 @@ class S_modul extends BaseController
 
     public function index()
     {
-        helper(['form', 'url']);
-        $this->Ms_modul = new Ms_modul();
-        $data['moduls'] = $this->Ms_modul->get_all_modul();
-        $data['kelasaktif'] = $this->session->get('nama');
-        $data['id_kelas'] = $this->session->get('id_kelas');
+        if (empty($this->session->get('id_kelas'))) {
+            $data['kelasaktif'] = 'BELUM TERDAFTAR';
+            $data['status_kelas'] = 0;
 
-        return view('t_siswa/modul', $data);
+            return view('t_siswa/kelas', $data);
+        } else {
+            helper(['form', 'url']);
+            $this->Ms_modul = new Ms_modul();
+            $data['moduls'] = $this->Ms_modul->get_all_modul();
+            $data['kelasaktif'] = $this->session->get('nama');
+            $data['id_kelas'] = $this->session->get('id_kelas');
+            $data['status'] = "active";
+
+            return view('t_siswa/modul', $data);
+        }
     }
 }
