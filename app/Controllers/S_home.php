58070608142983838f6490bc95d421c5431f5ac5
ratuverlaily <39119753+ratuverlaily@users.noreply.github.com>
2021-04-19 12:30:45 +0700

    Update S_home.php

--- app/Controllers/S_home.php
+++ app/Controllers/S_home.php
@@ -17,8 +17,15 @@ class S_home extends BaseController
 
     public function index()
     {
-        $data['kelasaktif'] = $this->session->get('nama');
-        $data['id_kelas'] = $this->session->get('id_kelas');
-        return view('t_siswa/home');
+        if (empty($this->session->get('id_kelas'))) {
+            $data['kelasaktif'] = 'BELUM TERDAFTAR';
+            $data['status_kelas'] = 0;
+
+            return view('t_siswa/kelas', $data);
+        } else {
+            $data['kelasaktif'] = $this->session->get('nama');
+            $data['id_kelas'] = $this->session->get('id_kelas');
+            return view('t_siswa/home', $data);
+        }
     }
 }
